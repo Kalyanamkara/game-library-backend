8e41e8285b64b7c9a7da25d33d0ca924630c594c
Kutay Acar <badempamacr@gmail.com>
2026-01-28 18:41:24 +0300

    security update

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from  passlib.context import CryptContext
+from pydantic import BaseModel
+from datetime import datetime ,timedelta
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+
+SECRET_KEY= "A*@49KDBA)9829e"
+ALGORITHM= "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 
 DATABASE_URL = "sqlite:///./oyunlar.db"
@@ -11,6 +28,12 @@ DATABASE_URL = "sqlite:///./oyunlar.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+class  UserDB(Base):
+    __tablename__ ="users"
+
+    id= Column(Integer, primary_key=True, index=True)
+    username=Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
 
 class OyunDB(Base):
@@ -42,17 +65,91 @@ class OyunGuncelle(BaseModel):
 app = FastAPI()
 
 
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+def sifrele(sifre: str):
+    return pwd_context.hash(sifre)
+def sifre_dogrula(duz_sifre, hashlenmis_sifre):
+    return pwd_context.verify(duz_sifre,hashlenmis_sifre)
+
+def token_olustur(data: dict):
+    to_encode= data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+        
+    
+    user = db.query(UserDB).filter(UserDB.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+
+
+
+    
+
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Biletiniz geçersiz veya süresi dolmuş",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user= db.query(UserDB).filter(UserDB.username == username).first()
+
+    if user is None:
+        raise credentials_exception
+    return user
+@app.post("/token")
+def giris_yap(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    kullanici =db.query(UserDB).filter(UserDB.username==form_data.username).first()
+
+    if not kullanici or not sifre_dogrula(form_data.password,kullanici.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Kullanici adi veya sifre hatali",
+            headers= {"WWW-Authenticate":"Bearer"},
+
+        )
+    access_token= token_olustur(data={"sub":kullanici.username})
+    return {"access_token": access_token, "token_type":"bearer"}
+
+@app.post("/register")
+def kayit_ol(kullanici:UserCreate, db: Session= Depends(get_db)):
+    sifreli_sifre = sifrele(kullanici.password)
+
+    yeni_kullanici =UserDB(username=kullanici.username, hashed_password=sifreli_sifre)
+    db.add(yeni_kullanici)
+    db.commit()
+    db.refresh(yeni_kullanici)
+
+    return{"Kullanici olusturuldu": yeni_kullanici.username}
 
 
 
 @app.post("/oyun-ekle")
-def oyun_ekle(oyun: OyunSema, db: Session = Depends(get_db)):
+def oyun_ekle(oyun: OyunSema, current_user: UserDB = Depends(get_current_user), db: Session = Depends(get_db)):
     
     yeni_oyun = OyunDB(isim=oyun.isim, tur=oyun.tur, puan=oyun.puan , yapımcı= oyun.yapımcı)
     db.add(yeni_oyun)  
@@ -67,7 +164,7 @@ def oyunlari_getir(db: Session = Depends(get_db)):
     return tum_oyunlar
 
 @app.delete("/oyun-sil/{oyun_id}")
-def oyun_sil(oyun_id: int, db: Session = Depends(get_db)):
+def oyun_sil(oyun_id: int, current_user: UserDB = Depends(get_current_user), db: Session = Depends(get_db)):
     
     silinecek_oyun = db.query(OyunDB).filter(OyunDB.id == oyun_id).first()
     
@@ -77,6 +174,11 @@ def oyun_sil(oyun_id: int, db: Session = Depends(get_db)):
     db.delete(silinecek_oyun)
     db.commit() 
     return {"bilgi": "Oyun başarıyla silindi", "silinen_id": oyun_id}
+
+
+@app.get("/ben-kimim")
+def profilimi_goster(current_user: UserDB = Depends(get_current_user)):
+    return {"kullanici_adi": current_user.username, "mesaj":  "Bu  mesajı sadece giriş yapanlar görebilir."}
 
 
 @app.put("/oyun-guncelle/{oyun_id}")
